b8eaeee3716a5cd75545a8f2cfd7910ba3171d5e
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-20 11:42:44 +0300

    ReactJS

---
+++ src/MyCustomComponent.js
@@ -0,0 +1,37 @@
+//First import modules/dependacies form React
+import React from "react";
+
+//Create the component logic in a function
+const MyCustomComponent = () =>{
+    return (
+        <div>
+        <p>My first components</p>
+
+        <ul>
+            <li>
+                ReactJS
+
+                
+            </li>
+
+            <a href="www.koko.com">Koko link</a>
+
+            <li>
+                AngularJS
+            </li>
+
+            <li>
+                VueJS
+            </li>
+        </ul>
+    </div>
+    
+    )
+    
+   
+    
+}
+
+//Export the component in order to be reusable from the another components in the application
+
+export default MyCustomComponent;
